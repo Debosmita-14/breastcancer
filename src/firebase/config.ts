61e49fcb9a3b503af495a914ccfb52e7b48a2be2
Debosmita Banerjee <banerjeedebosmita440@gmail.com>
2026-03-05 04:54:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9216096280-b0d80",
+  "appId": "1:178741982451:web:0774cd91d63bcb90057386",
+  "apiKey": "AIzaSyAxMXlt3Ds0RIh82wThcV06cfEi8vBq6v0",
+  "authDomain": "studio-9216096280-b0d80.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "178741982451"
+};
